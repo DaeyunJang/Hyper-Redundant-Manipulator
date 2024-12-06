6c7e0936e22bc211acdc94d26f1eb4e93c4ad619
DaeyunJang <bigyun9375@gmail.com>
2024-12-06 12:20:27 +0900

    update parameter and Controller stabilization

--- src/robot_control_pkg/include/robot_control_pkg/dynamics_parameters.hpp
+++ src/robot_control_pkg/include/robot_control_pkg/dynamics_parameters.hpp
@@ -19,9 +19,9 @@
 // #define KD  5.0
 
 // fast version MasterMACS 2000-100 - double of upper values
-#define KP  150.0
-#define KI  10.0
-#define KD  20.0
+#define KP  40.0
+#define KI  4.0
+#define KD  40.0
 #define SAMPLING_HZ  30.0    // Hz
 #define DT  1/SAMPLING_HZ  // sec
 // END - PID controller ================
